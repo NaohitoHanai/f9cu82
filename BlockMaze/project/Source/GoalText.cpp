1ed55573d9780ddfa71fc6b073bdfaedee6a7593
花井直人 <nao@hanamaru.games>
2024-11-21 11:10:33 +0900

    ゴールした時の表示と流れを入れました

--- BlockMaze/project/Source/GoalText.cpp
+++ BlockMaze/project/Source/GoalText.cpp
@@ -1,18 +1,43 @@
 #include "GoalText.h"
+#include "../Library/time.h"
 
 GoalText::GoalText()
 {
+	timer = 0.0f;
 }
 
 GoalText::~GoalText()
 {
 }
 
+int score = 100;
+
 void GoalText::Update()
 {
+	timer += Time::DeltaTime();
+	if (timer >= 2.5f) {
+		if (CheckHitKey(KEY_INPUT_SPACE)) {
+			SceneManager::ChangeScene("TitleScene");
+		}
+	}
 }
 
 void GoalText::Draw()
 {
-	DrawString(200, 200, "GOAL", GetColor(255, 255, 255));
+	int size = GetFontSize();
+
+	SetFontSize(50);
+	DrawString(200, 200, "GOAL", GetColor(255, 255, 127));
+	if (timer >= 1.0f) {
+		SetFontSize(25);
+		DrawFormatString(200, 300, GetColor(255, 255, 255),
+			"SCORE.....%6d", score);
+	}
+	if (timer >= 2.0f) {
+		DrawString(200, 500, "PUSH SPACE KEY", GetColor(255,255,255));
+	}
+	SetFontSize(size);
 }
+
+
+
--- BlockMaze/project/Source/GoalText.h
+++ BlockMaze/project/Source/GoalText.h
@@ -8,4 +8,6 @@ public:
 	~GoalText();
 	void Update() override;
 	void Draw() override;
+
+	float timer;
 };
